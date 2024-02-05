d113015c3db0263974f88983dd7b1afb7fe450ac
ivanbarboz <ibarbozaperu@gmail.com>
2024-02-05 15:33:11 -0500

    subiiendo mi pagina autoestudio

---
+++ demo.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>hola mundo</h1>
+    <h1></h1>
+    <?php
+    echo "<h1>esto es una prueba </h1>";
+    echo "aprendiendo php "
+    ?>
+</body>
+</html>
